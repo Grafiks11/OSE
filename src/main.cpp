b761f8994257ce61859992e8cb14809fa7ae4cad
Maciej Brzeski <maciej.brzeski@lpnplant.io>
2020-10-21 22:40:52 +0200

    main.cpp: use statically allocated c-string instead of String object

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 #define TEMP_PIN A0
 #define HEATER_PIN 1
 #define FAN_PIN 2
+#define BUFFER_LEN 64
 
 int temperature;
-String instruction;
+char buffer[BUFFER_LEN];
+
 void setup() {
   analogReference(INTERNAL); //Zmienia na 1.1V odniesienia
   Serial.begin(9600);
@@ -22,27 +24,28 @@ void setup() {
 void loop() {
   if (Serial.available() > 0)
   {
-    instruction=Serial.readStringUntil('\n');
-    if(instruction=="SET FAN")
+    Serial.readBytesUntil('\n',buffer,BUFFER_LEN);
+    if(strcasecmp(buffer,"SET FAN")==0)
     {
       digitalWrite(FAN_PIN,HIGH);
     }
-    else if(instruction=="RESET FAN")
+    else if(strcasecmp(buffer,"RESET FAN")==0)
     {
       digitalWrite(FAN_PIN,LOW);
     }
-    else if (instruction=="SET HEATER")
+    else if (strcasecmp(buffer,"SET HEATER")==0)
     {
        digitalWrite(HEATER_PIN,HIGH);
     }
-    else if (instruction=="RESET HEATER")
+    else if (strcasecmp(buffer,"RESET HEATER")==0)
     {
        digitalWrite(HEATER_PIN,LOW);
     }
-    else if (instruction=="GET TEMP")
+    else if (strcasecmp(buffer,"GET TEMP")==0)
     {
        temperature=analogRead(TEMP_PIN);
        Serial.println(temperature);
     }
-   }
+    memset(buffer,'\0',BUFFER_LEN);
+  }
 }
